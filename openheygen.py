e8a8617cc45448f518bb711b29eb38e9ce0f7924
xudong963 <wxd963996380@gmail.com>
2023-11-11 11:21:11 +0800

    first commit

---
+++ openheygen.py
@@ -0,0 +1,18 @@
+from TTS.api import TTS
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--text", type=str, default="Hello, world!")
+    parser.add_argument("--output_path", type=str, default="result/output.wav")
+    parser.add_argument("--speaker_wav", type=str, default="source/test.wav")
+    parser.add_argument("--language", type=str, default="en")
+    args = parser.parse_args()
+
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+    tts.tts_to_file(text=args.text,
+                    file_path=args.file_path,
+                    speaker_wav=args.speaker_wav,
+                    language=args.language)
+
+    print("Finish cloning the voice!")
---
+++ setup.sh
@@ -0,0 +1,8 @@
+conda create -n  python=3.8
+conda activate video-retalking
+
+conda install ffmpeg
+
+pip install -r requirements.txt
+
+
